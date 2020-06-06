7992ee2e0eee9d169fa83c785c847ccfdb2b1d12
Jamie vullo <jvullo@protonmail.com>
2020-06-06 14:34:53 -0400

    Added function to create enemy characters

--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -9,6 +9,12 @@
 import SpriteKit
 import GameplayKit
 
+enum Enemies {
+    case small
+    case medium
+    case large
+}
+
 class GameScene: SKScene {
     
     var tracksArray:[SKSpriteNode]? = [SKSpriteNode]()
@@ -34,6 +40,37 @@ class GameScene: SKScene {
         player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
         
         self.addChild(player!)
+        
+        let pulse = SKEmitterNode(fileNamed: "pulse")!
+        player?.addChild(pulse)
+        pulse.position = CGPoint(x: 0, y: 0)
+    }
+    
+    func createEnemy (type:Enemies, forTrack track:Int) -> SKShapeNode? {
+    let enemySprite = SKShapeNode()
+    
+    switch type {
+    case .small:
+        enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 70),
+                              cornerWidth: 8, cornerHeight:8, transform: nil)
+        enemySprite.fillColor = UIColor(red: 0.4431, green: 0.5529, blue: 0.7451, alpha: 1)
+    case .medium:
+        enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 100),
+                              cornerWidth: 8, cornerHeight:8, transform: nil)
+        enemySprite.fillColor = UIColor(red: 0.7804, green: 0.4039, blue: 0.4039, alpha: 1)
+    case .large:
+        enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 130),
+                              cornerWidth: 8, cornerHeight:8, transform: nil)
+        enemySprite.fillColor = UIColor(red: 0.7804, green: 0.6392, blue: 0.4039, alpha: 1)
+        
+        }
+        
+        guard let enemyPosition = tracksArray?[track].position else {return nil}
+        
+        enemySprite.position.x = enemyPosition.x
+        enemySprite.position.y = 50
+        
+        return enemySprite
     }
     
     override func didMove(to view: SKView) {
