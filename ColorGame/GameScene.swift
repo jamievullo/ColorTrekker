f0e76f32470a2921ae9e83254fcc805e1a907afe
Jamie vullo <jvullo@protonmail.com>
2020-06-06 11:23:31 -0400

    created player and player function to add to first track

--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -12,6 +12,7 @@ import GameplayKit
 class GameScene: SKScene {
     
     var tracksArray:[SKSpriteNode]? = [SKSpriteNode]()
+    var player:SKSpriteNode?
     
     func setupTracks(){
         for i in 0 ... 8 {
@@ -21,12 +22,20 @@ class GameScene: SKScene {
             
         }
     }
-    
+
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        guard let playerPosition = tracksArray?.first?.position.x else {return}
+        player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
+        
+        self.addChild(player!)
+    }
     
     override func didMove(to view: SKView) {
         setupTracks()
+        createPlayer()
         
-        tracksArray?.first?.color = UIColor.green
+//        tracksArray?.first?.color = UIColor.green
     }
     
     
