b21360e27fe0d63644f8d2a4f602c312b6f3c51e
Jamie vullo <jvullo@protonmail.com>
2020-06-06 16:51:14 -0400

    Another another commit for splitting up files for easy of reading

---
+++ ColorGame/GameElements.swift
@@ -0,0 +1,88 @@
+//
+//  GameElements.swift
+//  ColorGame
+//
+//  Created by Jamie vullo on 6/6/20.
+//  Copyright © 2020 Jamie vullo. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+enum Enemies:Int {
+    case small
+    case medium
+    case large
+}
+
+extension GameScene {
+    
+    func setupTracks(){
+        for i in 0 ... 8 {
+            if let track = self.childNode(withName: "\(i)") as? SKSpriteNode {
+                tracksArray?.append(track)
+            }
+            
+        }
+    }
+
+    func createPlayer() {
+        player = SKSpriteNode(imageNamed: "player")
+        
+        player?.physicsBody = SKPhysicsBody(circleOfRadius: player!.size.width / 2)
+        player?.physicsBody?.linearDamping = 0
+        player?.physicsBody?.categoryBitMask = playerCategory
+        player?.physicsBody?.collisionBitMask = 0
+        player?.physicsBody?.contactTestBitMask = enemyCategory | targetCategory
+        
+        guard let playerPosition = tracksArray?.first?.position.x else {return}
+        player?.position = CGPoint(x: playerPosition, y: self.size.height / 2)
+        
+        self.addChild(player!)
+        
+        let pulse = SKEmitterNode(fileNamed: "pulse")!
+        player?.addChild(pulse)
+        pulse.position = CGPoint(x: 0, y: 0)
+    }
+    
+    func createTarget() {
+        target = self.childNode(withName: "target") as? SKSpriteNode
+        target?.physicsBody = SKPhysicsBody(circleOfRadius: target!.size.width / 2)
+        target?.physicsBody?.collisionBitMask = 0
+    }
+    
+    func createEnemy (type:Enemies, forTrack track:Int) -> SKShapeNode? {
+        let enemySprite = SKShapeNode()
+            enemySprite.name = "ENEMY"
+        
+    switch type {
+        case .small:
+            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 70),
+                              cornerWidth: 8, cornerHeight:8, transform: nil)
+            enemySprite.fillColor = UIColor(red: 0.4431, green: 0.5529, blue: 0.7451, alpha: 1)
+        case .medium:
+            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 100),
+                              cornerWidth: 8, cornerHeight:8, transform: nil)
+            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.4039, blue: 0.4039, alpha: 1)
+        case .large:
+            enemySprite.path = CGPath(roundedRect: CGRect(x: -10, y: 0, width: 20, height: 130),
+                              cornerWidth: 8, cornerHeight:8, transform: nil)
+            enemySprite.fillColor = UIColor(red: 0.7804, green: 0.6392, blue: 0.4039, alpha: 1)
+        
+        }
+        
+        guard let enemyPosition = tracksArray?[track].position else {return nil}
+        
+        let up = directionArray[track]
+        
+        enemySprite.position.x = enemyPosition.x
+        enemySprite.position.y = up ? -130 : self.size.height + 130
+        
+        enemySprite.physicsBody = SKPhysicsBody(edgeLoopFrom: enemySprite.path!)
+        enemySprite.physicsBody?.categoryBitMask = enemyCategory
+        enemySprite.physicsBody?.velocity = up ? CGVector(dx:0, dy:velocityArray[track]) : CGVector(dx:0, dy:-velocityArray[track])
+        
+        return enemySprite
+    }
+    
+}
